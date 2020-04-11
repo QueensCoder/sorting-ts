d41c2c8e46b2fb96b805682879e5b835bb0cbd88
Ozal Khan <ozalkhan196@gmail.com>
2020-04-11 14:28:01 -0400

    added bubble sort

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,21 @@
-console.log('dog hi dog');
+class Sorter {
+  constructor(public collection: number[]) {}
 
-console.log('hello world');
-console.log('hello world');
-console.log('hello world');
-console.log('hello world');
+  bubbleSort(): void {
+    const { length } = this.collection;
+
+    for (let i = 0; i < length; i++) {
+      for (let j = 0; j < length - i - 1; j++) {
+        if (this.collection[j] > this.collection[j + 1]) {
+          let temp = this.collection[j];
+          this.collection[j] = this.collection[j + 1];
+          this.collection[j + 1] = temp;
+        }
+      }
+    }
+  }
+}
+
+const sorter = new Sorter([10, 3, -5, 0]);
+sorter.bubbleSort();
+console.log(sorter.collection);
