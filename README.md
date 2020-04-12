26337ee7cd7a34755fb60ccbac5faba91cc8104f
Ozal Khan <ozalkhan196@gmail.com>
2020-04-12 15:04:51 -0400

    refactored all classes to use abstract class Sorter instead of passing class to Sorter class, much cleaner implementtion

--- README.md
+++ README.md
@@ -4,3 +4,10 @@ typeguards - typeof for primatives, instanceof for others
 
 instanceof - allows us to type guard agaisnt types when using unions
 | - unions that allow us to accept multiple types
+
+Abstract classes
+
+    cannot create objs directly
+    used as parent class
+    can contain real methods
+    children of this class has to promise to have the methods mentioned
--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,22 @@
-import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
 import { CharacterCollection } from './CharacterCollection';
 import { LinkedList } from './LinkedList';
+
 // sort numbers
 const numberCollection = new NumbersCollection([10, 3, -5, 0]);
-
-const sorter = new Sorter(numberCollection);
-sorter.bubbleSort();
+numberCollection.bubbleSort();
 console.log(numberCollection.data);
 
-// sort characters
+// // sort characters
 const charCollection = new CharacterCollection('Xaayb');
-const sortChar = new Sorter(charCollection);
-
-sortChar.bubbleSort();
+charCollection.bubbleSort();
 console.log(charCollection.data);
 
+// sort values in a linked list
 const listValues = [500, -10, -3, 4];
 const linkedList = new LinkedList();
 
 listValues.forEach((value) => linkedList.add(value));
 
-const listSorter = new Sorter(linkedList);
-
-listSorter.bubbleSort();
+linkedList.bubbleSort();
 linkedList.print();
