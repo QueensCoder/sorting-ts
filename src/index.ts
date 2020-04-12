9d02553059c5527c65ebf3124edd255e6a990c24
Ozal Khan <ozalkhan196@gmail.com>
2020-04-12 14:23:07 -0400

    linked list is now sortable using bubble sort method

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
 import { CharacterCollection } from './CharacterCollection';
-
+import { LinkedList } from './LinkedList';
 // sort numbers
 const numberCollection = new NumbersCollection([10, 3, -5, 0]);
 
@@ -15,3 +15,13 @@ const sortChar = new Sorter(charCollection);
 
 sortChar.bubbleSort();
 console.log(charCollection.data);
+
+const listValues = [500, -10, -3, 4];
+const linkedList = new LinkedList();
+
+listValues.forEach((value) => linkedList.add(value));
+
+const listSorter = new Sorter(linkedList);
+
+listSorter.bubbleSort();
+linkedList.print();
